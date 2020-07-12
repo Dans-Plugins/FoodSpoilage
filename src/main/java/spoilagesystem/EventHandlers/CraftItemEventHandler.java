395e63ec3ad238267e1b82b2104fce76731fdba0
dmccoystephenson <dmccoystephenson@gmail.com>
2020-07-12 16:23:14 -0600

    passed an instance of main to CraftItemEventHandler class

--- src/main/java/spoilagesystem/EventHandlers/CraftItemEventHandler.java
+++ src/main/java/spoilagesystem/EventHandlers/CraftItemEventHandler.java
@@ -1,5 +1,18 @@
 package spoilagesystem.EventHandlers;
 
+import org.bukkit.event.inventory.CraftItemEvent;
+import spoilagesystem.Main;
+
 public class CraftItemEventHandler {
+
+    Main main = null;
+
+    public CraftItemEventHandler(Main plugin) {
+        main = plugin;
+    }
+
+    public void handle(CraftItemEvent event) {
+
+    }
 
 }
--- src/main/java/spoilagesystem/Main.java
+++ src/main/java/spoilagesystem/Main.java
@@ -1,9 +1,12 @@
 package spoilagesystem;
 
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import spoilagesystem.EventHandlers.CraftItemEventHandler;
 import spoilagesystem.Subsystems.TimeStampSubsystem;
 
-public final class Main extends JavaPlugin {
+public final class Main extends JavaPlugin implements Listener {
 
     public String version = "v0.1";
 
@@ -12,11 +15,16 @@ public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
+        this.getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
 
+    }
+
+    public void onCraft(CraftItemEvent event) {
+        CraftItemEventHandler handler = new CraftItemEventHandler(this);
+        handler.handle(event);
     }
 }
