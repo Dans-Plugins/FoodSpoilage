a5550b7f326668e3758e8a16791d2c9a1f92e9b1
dmccoystephenson <dmccoystephenson@gmail.com>
2020-07-12 15:27:18 -0600

    initial commit

---
+++ src/main/java/spoilagesystem/Main.java
@@ -0,0 +1,19 @@
+package spoilagesystem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    public String version = "v0.1";
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
