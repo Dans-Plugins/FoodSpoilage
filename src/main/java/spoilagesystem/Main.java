1125008bf76c6993a95de0243ae901368cd0653b
dmccoystephenson <dmccoystephenson@gmail.com>
2020-07-16 23:07:51 -0600

    incremented version

--- src/main/java/spoilagesystem/Main.java
+++ src/main/java/spoilagesystem/Main.java
@@ -19,7 +19,7 @@ import spoilagesystem.Subsystems.TimeStampSubsystem;
 
 public final class Main extends JavaPlugin implements Listener {
 
-    public String version = "v1.3.1";
+    public String version = "v1.3.2";
 
     // subsystems
     public TimeStampSubsystem timestamp = new TimeStampSubsystem(this);
